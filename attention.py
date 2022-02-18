d8ba604bb94885d38f230201ce722b722e55ec51
rick <rick.wainwright2@gmail.com>
2022-02-18 11:33:37 +1000

    add GuidedIG

--- attention.py
+++ attention.py
@@ -18,15 +18,16 @@ import saliency.core as saliency
 
 # Set locations of files
 IMG_DIR = "./ChestX-ray14/images/"
-DATA = "./sample.csv"
-WEIGHTS = "./complete/666104_IV2_no_CM_no_NF/model.InceptionV3.h5"
+DATA = "./ChestX-ray14/Data_Entry_2017_v2020.csv"
+UNCERTAINTY = "./20220207_1307_uncertainty_InceptionV3.csv"
+WEIGHTS = "./complete/665412_softmax_no_NF/model.InceptionV3.h5"
 
 # Set training variables
-to_figure = True
-to_images = False
+to_figure = False
+to_images = True
 AVAILABLE_MODELS = ["InceptionV3", "InceptionResNetV2"]
 CURRENT_MODEL = AVAILABLE_MODELS[0]  # Select from AVAILABLE_MODELS
-ONLY_FINDINGS = False
+ONLY_FINDINGS = True
 NO_COMORBID = False
 
 OUTPUT_SIZE = 299  # height is the same as width
@@ -38,24 +39,6 @@ if CURRENT_MODEL not in AVAILABLE_MODELS:
     exit()
 
 start = time.time()
-
-# Load NIH data
-df = pd.read_csv(DATA)
-
-# Get all possible finding labels
-labels = get_labels(df, only_findings=ONLY_FINDINGS)
-print(labels)
-
-# Update data
-df = prepare_df(df, labels, IMG_DIR, only_findings=ONLY_FINDINGS, no_comorbid=NO_COMORBID)
-
-# Create generators for each split
-core_idg = get_idg()
-img_gen = get_generator_from_df(core_idg, df, BATCH_SIZE, labels, OUTPUT_SIZE, shuffle=False)
-print("DataFrame complete")
-
-# Get model and optimiser
-model, optimizer = get_model(CURRENT_MODEL, len(labels), OUTPUT_SIZE, weights=WEIGHTS)
 
 
 def call_model_function(images, call_model_args=None, expected_keys=None):
@@ -98,6 +81,35 @@ def get_original(file_path):
     return im
 
 
+# Load NIH data
+data_df = pd.read_csv(DATA)
+uncertainty_df = pd.read_csv(UNCERTAINTY)
+
+# Get all possible finding labels
+labels = get_labels(data_df, only_findings=ONLY_FINDINGS)
+print(labels)
+
+# Update data
+data_df = prepare_df(data_df, labels, IMG_DIR, only_findings=ONLY_FINDINGS, no_comorbid=NO_COMORBID)
+data_df.set_index("Image Index", inplace=True)
+uncertainty_df.set_index("Image Index", inplace=True)
+print(len(data_df))
+print(len(uncertainty_df))
+
+df = data_df.join([uncertainty_df], how="inner")
+
+df = df.sort_values(by="Normalised PH")
+df.to_csv("20220215_All_Uncertainties.csv")
+print(len(df))
+
+# Create generators for each split
+core_idg = get_idg()
+img_gen = get_generator_from_df(core_idg, df, BATCH_SIZE, labels, OUTPUT_SIZE, shuffle=False)
+print("DataFrame complete")
+
+# Get model and optimiser
+model, optimizer = get_model(CURRENT_MODEL, len(labels), OUTPUT_SIZE, weights=WEIGHTS)
+
 top_row = ["Image Index"] + labels + ["Prediction"]
 predictions_df = pd.DataFrame(columns=top_row)
 
@@ -125,30 +137,37 @@ for i in range(len(df) - 1):
 
     # Construct the saliency object. This alone doesn't do anything.
     integrated_gradients = saliency.IntegratedGradients()
+    guided_ig = saliency.GuidedIG()
 
     # Baseline is a black image.
     baseline = np.zeros(im.shape)
 
+    ### INTERGRATED GRADIENTS ###
     # Compute the vanilla mask and the smoothed mask.
     vanilla_integrated_gradients_mask_3d = integrated_gradients.GetMask(
         im, call_model_function, call_model_args, x_steps=25, x_baseline=baseline, batch_size=20)
     # Smoothed mask for integrated gradients will take a while since we are doing nsamples * nsamples computations.
     smoothgrad_integrated_gradients_mask_3d = integrated_gradients.GetSmoothedMask(
         im, call_model_function, call_model_args, x_steps=25, x_baseline=baseline, batch_size=20)
+    # Guided mask
+    guided_ig_mask_3d = guided_ig.GetMask(
+        im, call_model_function, call_model_args, x_steps=25, x_baseline=baseline, max_dist=1.0, fraction=0.5)
 
     # Call the visualization methods to convert the 3D tensors to 2D grayscale.
     vanilla_mask_grayscale = saliency.VisualizeImageGrayscale(vanilla_integrated_gradients_mask_3d)
     smoothgrad_mask_grayscale = saliency.VisualizeImageGrayscale(smoothgrad_integrated_gradients_mask_3d)
+    guided_ig_mask_grayscale = saliency.VisualizeImageGrayscale(guided_ig_mask_3d)
 
     if to_images:
         # save images
-        save_image(vanilla_mask_grayscale, f"./vanilla_IG/{img_name}")
-        save_image(smoothgrad_mask_grayscale, f"./smoothgrad_IG/{img_name}")
+        save_image(vanilla_mask_grayscale, f"./maps/vanilla_IG/{img_name}")
+        save_image(smoothgrad_mask_grayscale, f"./maps/smoothgrad_IG/{img_name}")
+        save_image(guided_ig_mask_grayscale, f"./maps/guided_IG/{img_name}")
 
     if to_figure:
         # Set up matplot lib figures.
         ROWS = 1
-        COLS = 3
+        COLS = 4
         UPSCALE_FACTOR = 10
 
         title = f"{img_name} - True: {true_findings}; Predicted: {predicted_findings} ({np.amax(predictions[0]):.2f})"
@@ -159,6 +178,7 @@ for i in range(len(df) - 1):
         # Render the saliency masks.
         show_image(vanilla_mask_grayscale, title='Vanilla Integrated Gradients', ax=plt.subplot(ROWS, COLS, 2))
         show_image(smoothgrad_mask_grayscale, title='Smoothgrad Integrated Gradients', ax=plt.subplot(ROWS, COLS, 3))
+        show_image(guided_ig_mask_grayscale, title='Guided Integrated Gradients', ax=plt.subplot(ROWS, COLS, 4))
 
         ax = plt.subplot(ROWS, COLS, 1)
         plt.axis('off')
@@ -172,6 +192,6 @@ for i in range(len(df) - 1):
         print(f"{predictions[0]}")
 
 predictions_df.set_index("Image Index", inplace=True)
-predictions_df.to_csv(f"{time.strftime('%Y%m%d_%H%M')}_tsne_predictions.csv")
+predictions_df.to_csv(f"{time.strftime('%Y%m%d_%H%M')}_attention_predictions.csv")
 
 print(time.time() - start)
